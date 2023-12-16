530da5121309e5bd1f22799059da6a1c086a10b1
FRosty799 <fauzanramdhani7@gmail.com>
2023-12-16 23:41:57 +0700

    Update App.java

--- App.java
+++ App.java
@@ -1,5 +1,25 @@
 package UAS.Project;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import UAS.Project.Control.Controller;
+import UAS.Project.Model.Buku;
+import UAS.Project.Model.Customer;
+import UAS.Project.Model.Librarian;
+
 public class App {
-    
+    public static void main(String[] args) throws Exception {
+        Controller LibControl = new Controller();
+        LibControl.setUp();
+        boolean cont = true;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (cont) {
+            
+        }
+
+    }
 }
